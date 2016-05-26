4597b5303ae0e1be27c1534cd24ed66f88a143b7
domson <sondermann@fh-muenster.de>
2016-05-26 11:17:15 +0200

    Armour class added

---
+++ src/main/java/de/domson/midgard/pojos/Armour.java
@@ -0,0 +1,15 @@
+package de.domson.midgard.pojos;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by domson on 26.05.16.
+ */
+
+public class Armour {
+
+    private String ArmourClass;
+    private int healthPointsBonus;
+    private int rangeMalus;
+    private int agilityMalus;
+}
